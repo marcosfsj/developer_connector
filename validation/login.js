a4c7fd9c161cfa93ede9caf6ca202e30aa8a26b1
marcfernandes <marcfernandes@teksystems.com>
2019-02-22 16:36:10 -0500

    Adds endpoints for user registration and login

---
+++ validation/login.js
@@ -0,0 +1,23 @@
+const Validator = require("validator");
+const _ = require("lodash");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  data.email = data.email ? data.email : "";
+  data.password = data.password ? data.password : "";
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  return { errors, isValid: _.isEmpty(errors) };
+};
