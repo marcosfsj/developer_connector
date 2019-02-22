cf60a9366e87914d6c0279f2e907f8b16c67b632
marcfernandes <marcfernandes@teksystems.com>
2019-02-22 13:13:55 -0500

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://marcos:Ma78juma@cluster0-shard-00-00-e9l05.mongodb.net:27017,cluster0-shard-00-01-e9l05.mongodb.net:27017,cluster0-shard-00-02-e9l05.mongodb.net:27017/devconnector?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+};
